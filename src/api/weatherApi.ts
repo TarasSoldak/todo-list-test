cc354d62f2273ba3af9da880ce8eacab5ec4392a
TarasSoldak <soldaktaras@gmail.com>
2022-07-31 14:37:12 +0300

    fourth commit

--- src/api/weatherApi.ts
+++ src/api/weatherApi.ts
@@ -6,7 +6,7 @@ import { IW } from "../store/reducers/weatherSlice";
 export const getWeather = async () => {
   try {
     const response = await axios
-      .get<IW>(`http://api.openweathermap.org/data/2.5/weather?q=London&appid=12de24d1625ed8b9e67c4b7ee4eb146d`);
+      .get<IW>(`https://api.openweathermap.org/data/2.5/weather?q=London&appid=12de24d1625ed8b9e67c4b7ee4eb146d`);
     return response
   } catch (error) {
     console.error(error);
